25034e8d26e4872aa95ba3a4070a3fe518024688
Dvitash <dvitash3414@gmail.com>
2021-05-14 14:03:47 -0400

    finished a bunch of stuff, basic searching need to clean site up then done i guess

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 console.log("connected");
+
+async function getapi() {
+    
+    var response = await fetch('https://api.lyrics.ovh/v1/' + document.getElementById("artist").value +"/" + document.getElementById("title").value);
+    
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        document.getElementById("results").innerHTML = data.lyrics;
+    }
+}
