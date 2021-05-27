b41185d33e263fbbd52abddcb56d4383c0cad809
Dvitash <dvitash3414@gmail.com>
2021-05-27 12:05:32 -0400

    final commit project is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,23 @@
 console.log("connected");
 
 async function getapi() {
-    
+
+    if(document.getElementById("artist").value != "" && document.getElementById("title").value != "")
+    {
+        document.getElementById("results").innerHTML = "Loading...";
+    }
+
+    console.log("Fetching lyrics from Artist " + document.getElementById("artist").value + " and song title " + document.getElementById("title").value);
     var response = await fetch('https://api.lyrics.ovh/v1/' + document.getElementById("artist").value +"/" + document.getElementById("title").value);
+    console.log("done");
     
     var data = await response.json();
     console.log(data);
-    if (response) {
+    if (data.lyrics != "") {
         document.getElementById("results").innerHTML = data.lyrics;
+    }
+    else
+    {
+        document.getElementById("results").innerHTML = "ERROR: Lyrics not found";
     }
 }
